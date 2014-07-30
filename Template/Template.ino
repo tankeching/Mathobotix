7cc3dfb26c981b7bf29649d0e1f684d818851d4b
Kenneth <tankeching@gmail.com>
2014-07-30 10:42:40 -0700

    Arduino Code Template
    Contains:
    void setup
    void loop

---
+++ Template/Template.ino
@@ -0,0 +1,18 @@
+//This is the basic outline of a arduino code.
+
+
+void setup(){
+ 
+ //Put all prototypes and setup commands here.
+  
+}
+
+
+
+void loop(){
+  
+  
+  //This is where youre main code goes.
+  
+  
+}
